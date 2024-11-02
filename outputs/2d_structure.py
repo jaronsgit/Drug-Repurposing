88d5c8b8914fe5be7b51eedf75ce4083cb969d62
Paula Cordero Encinar <pc2222@ic.ac.uk>
2024-11-02 15:34:01 +0000

    code for 2d structure

---
+++ outputs/2d_structure.py
@@ -0,0 +1,12 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+
+smiles = "CCC(CC)COC(=O)[C@H](C)N[P@](=O)(OC[C@@H]1[C@H]([C@H]([C@](O1)(C#N)C2=CC=C3N2N=CN=C3N)O)O)OC4=CC=CC=C4"  
+
+molecule = Chem.MolFromSmiles(smiles)
+
+Draw.MolToImage(molecule)
+
+image = Draw.MolToImage(molecule)
+
+image.save("molecule_structure.pdf", "PDF")
